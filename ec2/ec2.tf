f4ddf519affaa760dd637525a45b227fa04c3e8d
inboxtoshashi.github.io <inboxtoshashi@gmail.com>
2025-05-13 14:44:36 +0530

    added auto ssm-param creation along with roles & policies

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "public_ec2" {
   name                   = "url_app"
   ami                    = var.ami
   key_name               = var.ssh_key
+  iam_instance_profile   = var.iam_instance_profile_name
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [var.vpc_security_group_ids]
   tags = {
@@ -19,4 +20,9 @@ resource "aws_instance" "public_ec2" {
       agent       = false
     }
   }
+}
+
+variable "iam_instance_profile_name" {
+  type        = string
+  description = "IAM instance profile name for EC2"
 }
---
+++ ssm-param/jenkins_secret.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Generate random 8-character alphanumeric password
+JENKINS_SECRET=$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 8)
+
+# Parameter name
+PARAM_NAME="/jenkins/secret"
+
+# AWS region
+AWS_REGION="us-east-1"
+
+echo "[+] Creating SSM parameter: $PARAM_NAME"
+aws ssm put-parameter \
+  --name "$PARAM_NAME" \
+  --value "$JENKINS_SECRET" \
+  --type "SecureString" \
+  --overwrite \
+  --region "$AWS_REGION"
+
+echo "[✔] Secret stored successfully in SSM."
